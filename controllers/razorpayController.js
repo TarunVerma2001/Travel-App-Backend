2a25aa758f47f8c9835a2dd94789da570fee4c1a
TarunVerma2001 <verma.tarun200109@gmail.com>
2021-12-31 21:51:58 +0530

    razorpay init2

--- controllers/razorpayController.js
+++ controllers/razorpayController.js
@@ -23,6 +23,7 @@ exports.razorpayIntegration = async (req, res, next) => {
 };
 exports.verifyPayment = async (req, res, next) => {
   try {
+    console.log('web verification');
     console.log(req.body);
     res.status(200).json({
       status: 'success',
